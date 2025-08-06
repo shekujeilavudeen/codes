5ff79fc9898931276a662f6d7c630c470bc4720e
shekujeilavudeen <shekujeilavudeen9501@mountzion.ac.in>
2025-08-06 13:52:34 +0530

    Create vishnu.java

---
+++ vishnu.java
@@ -0,0 +1,26 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    int number =22;
+
+    
+
+    if (number <0) {
+
+      System.out.println(number + " is positive.");
+
+    } else if (number < 0) {
+
+      System.out.println(number + " is negative.");
+
+    } else {
+
+      System.out.println(number + " is zero.");
+
+    }
+  }
+    
+    
+    
+    
